b64f33589d549929de5ef2cbd318ec485efdb35a
tsabelmann <tim.sabelmann@hotmail.de>
2020-12-04 19:23:55 +0100

    doc: Added comments to coroutine 'play_sound'

--- cthulhu_discord_bot/__init__.py
+++ cthulhu_discord_bot/__init__.py
@@ -364,12 +364,19 @@ async def play_sound(ctx, file_path):
             and hasattr(author, 'voice') \
             and author.voice is not None \
             and author.voice.channel is not None:
+        # Get voice channel the author is in
         voice_channel = ctx.author.voice.channel
+
+        # Get the (output) voice of the bot
         voice = ctx.channel.guild.voice_client
         if voice is None:
+            # Join voice channel of the author and get the (output) voice
             voice = await voice_channel.connect()
         elif voice.channel != voice_channel:
+            # Change (output) voice of bot if it is on a different channel
             await voice.move_to(voice_channel)
+
+        # Play audio
         voice.play(discord.FFmpegPCMAudio(file_path))
     else:
         pass
