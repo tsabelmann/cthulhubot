fb61d009d57cea36d07c2e98c10dedf4ff9d3122
tsabelmann <tim.sabelmann@hotmail.de>
2022-12-19 20:31:26 +0100

    feat: Upgraded cthulhubot implementation

--- cthulhubot/backend/bot.py
+++ cthulhubot/backend/bot.py
@@ -7,14 +7,20 @@ import disnake
 
 
 class CthulhuBot(Bot):
-    def __init__(self):
+    def __init__(self, debug: bool = False):
+        # Command sync
         command_sync_flags = commands.CommandSyncFlags.all()
         command_sync_flags.sync_commands = True
+        command_sync_flags.sync_commands_debug = debug
+
+        # Intents
+        intents = disnake.Intents.default()
+        intents.message_content = True
 
         super().__init__(
             command_prefix=["#", "/"],
             help_command=None,
-            intents=disnake.Intents.default(),
+            intents=intents,
             command_sync_flags=command_sync_flags,
             activity=disnake.Activity(name="Call of Cthulhu", type=disnake.ActivityType.playing)
         )
