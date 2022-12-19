ffdbf66877790d163d2604b0fa3ee75f63c7f389
tsabelmann <tim.sabelmann@hotmail.de>
2022-12-19 21:03:20 +0100

    feat: Updated cli.py to support legacy, dice, and probe commands

--- cthulhubot/frontend/cli.py
+++ cthulhubot/frontend/cli.py
@@ -3,15 +3,33 @@
 
 import os
 
-from cthulhubot import client
+from cthulhubot.backend.bot import CthulhuBot
+from cthulhubot.backend.cogs import Legacy, Dice, ProbeCog
 from dotenv import load_dotenv
+
+from disnake.ext.commands import Bot
+from disnake.ext import commands
+import disnake
 
 
 def main():
+    bot = CthulhuBot()
+    bot.i18n.load("locale/")
+
+    bot.add_cog(Legacy(bot))
+    bot.add_cog(Dice(bot))
+    bot.add_cog(ProbeCog(bot,
+                         success_sound_paths=["sounds/success.m4a"],
+                         failure_sound_paths=["sounds/fail.m4a"],
+                         play_sounds=True)
+    )
+
     # Load environment variables from .env file
     load_dotenv()
 
     # Get token from environment
     token = os.getenv("DISCORD_TOKEN")
 
-    client.run(token)
+    print(bot.commands)
+
+    bot.run(token)
