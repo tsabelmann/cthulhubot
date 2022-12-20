8e5ea8e4e2d31d1837cc5b74b93949d667dcf0f4
tsabelmann <tim.sabelmann@hotmail.de>
2022-12-20 13:02:52 +0100

    init: Added __init__.py

--- cthulhubot/backend/cogs/__init__.py
+++ cthulhubot/backend/cogs/__init__.py
@@ -1,6 +1,6 @@
 """
 """
 
-from cthulhubot.backend.cogs.legacy import Legacy
+from cthulhubot.backend.cogs.legacy import LegacyCog
 from cthulhubot.backend.cogs.dice import Dice
 from cthulhubot.backend.cogs.probe import ProbeCog
