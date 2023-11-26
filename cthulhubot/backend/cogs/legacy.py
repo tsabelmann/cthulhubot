1ce79278ab5c1e1752423a896c5a03b1527fb72a
tsabelmann <tim.sabelmann@hotmail.de>
2023-11-26 14:44:51 +0100

    feat: Updated legacy cog to support the probe and roll commands

--- cthulhubot/backend/cogs/legacy.py
+++ cthulhubot/backend/cogs/legacy.py
@@ -7,8 +7,18 @@ from cthulhubot.backend.sound import play_sound
 from disnake.ext import commands
 from pathlib import Path
 
+import re
 import random
 import typing
+
+
+__dice_regex__ = re.compile(
+    r'(\s*[+\-]?\s*[1-9][0-9]*([dDwW][1-9][0-9]*)?)+'
+)
+
+__die_regex__ = re.compile(
+    r'(?P<sign>[+\-])?\s*(?P<value>[1-9][0-9]*)([dDwW](?P<dvalue>[1-9][0-9]*))?'
+)
 
 
 class LegacyCog(commands.Cog):
@@ -23,11 +33,67 @@ class LegacyCog(commands.Cog):
         self._failure_sound_paths = failure_sound_paths
         self._play_sounds = play_sounds
 
-    @commands.command(name="roll")
+    @commands.command(name="roll", aliases=["r"])
     async def roll(self, ctx: commands.Context):
-        await ctx.send(f"arg=")
+        message = ctx.message.content
+        match = __dice_regex__.search(message)
 
-    @commands.command(name="probe")
+        if match:
+            # List of random dice
+            rng_numbers = []
+
+            # List of additions
+            additions = []
+            for m in __die_regex__.finditer(message):
+                # Get sign
+                sign = m.group("sign")
+                if sign is None or sign == "+":
+                    sign = 1
+                else:
+                    sign = -1
+
+                # Get addition or amount of dice
+                value = m.group("value")
+                value = int(value)
+
+                # Get die eye value
+                dvalue = m.group("dvalue")
+
+                # Decide whether value is additional value or die
+                if dvalue is not None:
+                    # Compute dvalue (int) from dvalue (str)
+                    dvalue = int(dvalue)
+
+                    # Append dice to rng_numbers
+                    rng_numbers.append([sign * random.randint(1, dvalue) for _ in range(value)])
+                else:
+                    # Append additional value
+                    additions.append(sign * value)
+
+            # Add random dice to sum
+            rng_sum = sum([sum(rng) for rng in rng_numbers])
+
+            # Add additional values to sum
+            add_sum = sum(additions)
+
+            # Total sum
+            total_sum = rng_sum + add_sum
+
+            # Get author nick name preferably, or, the user name secondly
+            author_name = ctx.message.author.nick or ctx.message.author.name
+
+            # Construct message
+            await ctx.send(f"""**{author_name}**\n"""
+                        f"""Roll: {''.join([str(rng) for rng in rng_numbers])}\n"""
+                        f"""Sum: **{rng_sum}**\n"""
+                        f"""Additions: {additions}\n"""
+                        f"""Sum: **{add_sum}**\n"""
+                        f"""Result: **{total_sum}**""")
+        else:
+            return
+        
+    
+    @commands.command(name="probe", aliases=["p"])
     async def probe(self, ctx: commands.Context, *args: str):
         content = ' '.join(args)
 
